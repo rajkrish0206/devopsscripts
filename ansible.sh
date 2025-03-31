b22dc4d9f3e974611cfd313ce452304144a2b107
rajkrish0206 <rajkrish0206@gmail.com>
2025-03-31 09:02:12 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,5 @@
 amazon-linux-extras install ansible2 -y
 yum install python3 python-pip python-dlevel -y
+
+sed -i 's/present/absent/g' roles/packages/tasks/main.yml
+find . -type f -exec sed 's/present/absent/g' {}\;
